e0f670db7de9c7ba0ec40b92438c530bf025308d
Kaltura CSI <community@kaltura.com>
2018-06-22 04:20:33 -0400

    New clientlib ver

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -92,8 +92,8 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:18-06-21');
-		$this->setApiVersion('3.3.0');
+		$this->setClientTag('php5:18-06-22');
+		$this->setApiVersion('14.1.0');
 		
 		$this->baseEntry = new Kaltura_Client_BaseEntryService($this);
 		$this->category = new Kaltura_Client_CategoryService($this);
--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP API Client Library for Zend framework.
-Compatible with Kaltura server version 3.3.0 and above.
+Compatible with Kaltura server version 14.1.0 and above.

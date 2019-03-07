c623320c4f8b117d03cd814b61ce8d2cd2a4c62a
Kaltura CSI <community@kaltura.com>
2019-03-07 03:50:27 -0500

    New clientlib ver

---
+++ Kaltura/Client/Reach/Enum/CatalogItemLanguage.php
@@ -0,0 +1,57 @@
+<?php
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2019  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class Kaltura_Client_Reach_Enum_CatalogItemLanguage extends Kaltura_Client_EnumBase
+{
+	const AR = "Arabic";
+	const YUE = "Cantonese";
+	const ZH = "Chinese";
+	const NL = "Dutch";
+	const EN = "English";
+	const EN_US = "English (American)";
+	const EN_GB = "English (British)";
+	const FR = "French";
+	const DE = "German";
+	const HE = "Hebrew";
+	const HI = "Hindi";
+	const IT = "Italian";
+	const JA = "Japanese";
+	const KO = "Korean";
+	const CMN = "Mandarin Chinese";
+	const PT = "Portuguese";
+	const RU = "Russian";
+	const ES = "Spanish";
+	const TH = "Thai";
+	const TR = "Turkish";
+}
+

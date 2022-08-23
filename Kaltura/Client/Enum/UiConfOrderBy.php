acf204dcd50dce4144f7aadbcb73acccd64f4744
Kaltura CSI <community@kaltura.com>
2022-08-22 20:06:14 -0400

    New clientlib ver

--- Kaltura/Client/Enum/UiConfOrderBy.php
+++ Kaltura/Client/Enum/UiConfOrderBy.php
@@ -34,8 +34,12 @@
 class Kaltura_Client_Enum_UiConfOrderBy extends Kaltura_Client_EnumBase
 {
 	const CREATED_AT_ASC = "+createdAt";
+	const ID_ASC = "+id";
+	const NAME_ASC = "+name";
 	const UPDATED_AT_ASC = "+updatedAt";
 	const CREATED_AT_DESC = "-createdAt";
+	const ID_DESC = "-id";
+	const NAME_DESC = "-name";
 	const UPDATED_AT_DESC = "-updatedAt";
 }
 
--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP API Client Library for Zend framework.
-Compatible with Kaltura server version 18.12.0 and above.
+Compatible with Kaltura server version 18.13.0 and above.
